554d0b518fa8c004c0b1b16083e4605d3a6fdcda
Sean Ferguson <sean@sgnl24.com>
2026-07-13 15:41:05 -0400

    update otel bootstrap

--- packages/api/src/bootstrap.ts
+++ packages/api/src/bootstrap.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // OTEL must init before any other imports
-import('@zyno-io/ts-server-foundation/telemetry/otel/index.js').then(async otel => {
+import('@zyno-io/ts-server-foundation/otel').then(async otel => {
     otel.init();
     await import('./app.js');
 });
